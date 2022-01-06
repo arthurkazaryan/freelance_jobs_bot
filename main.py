2525f5098c18132d51612e64034ef3984bb4a228
arthurkazaryan <arthur.kazaryan@yahoo.com>
2022-01-06 20:39:28 +0300

    Added schedule.
    Messages are now prepared only between 12PM and 12AM every 3-5 minutes.

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import os
 import telebot
-import time
+from time import sleep
+import datetime
+from random import randint
+from pytz import timezone
 from dotenv import load_dotenv, find_dotenv
 from upwork.parse import prepare_message
 
@@ -12,11 +15,13 @@ bot = telebot.TeleBot(API_TOKEN)
 @bot.message_handler(commands=['start'])
 def hello(message):
     while True:
-        new_messages = prepare_message()
-        if new_messages:
-            for msg in new_messages:
-                bot.send_message(message.chat.id, msg)
-        time.sleep(60)
+        current_time = datetime.datetime.now().astimezone(timezone("Europe/Moscow"))
+        if current_time.hour in range(12, 24):
+            new_messages = prepare_message()
+            if new_messages:
+                for msg in new_messages:
+                    bot.send_message(message.chat.id, msg)
+        sleep(randint(177, 325))
 
 
 bot.polling()
